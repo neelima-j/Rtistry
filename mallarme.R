5a9795bf23c7a145f745c499b614366ed4eb0c38
Neelima J <78197684+neelima-j@users.noreply.github.com>
2021-06-06 08:26:23 +0530

    Update mallarme.R

--- mallarme.R
+++ mallarme.R
@@ -6,11 +6,9 @@ library(ggforce)
 # 1991
 # https://www.moma.org/collection/works/90695
 
-green <- "#71A119"
-red <- "#D5341B"
-blue <- "#0F4999"
 black <- "#2B2828"
 background <- "#E5DECD"
+grey <- "#808080"
 
 line <- data.frame(
   x1 = c(0,10),
@@ -19,12 +17,20 @@ line <- data.frame(
   yend = c(-10,0)
 )
 
-p1 <- ggplot() + geom_arc(aes(x0 = 0, y0 = 0, r = 10, start = pi, end = pi/2))
 
-df_poly1 <- rbind(c(0,0),data.frame(x = ggplot_build(p1)$data[[1]]$x,y = ggplot_build(p1)$data[[1]]$y),c(0,0))
+# The arc
+p1 <- ggplot() + 
+      geom_arc(aes(x0 = 0, y0 = 0, r = 10, start = pi, end = pi/2))
+
+# Making the polygon with the arc
+df_poly1 <- rbind(c(0,0),
+                  data.frame(x = ggplot_build(p1)$data[[1]]$x,
+                             y = ggplot_build(p1)$data[[1]]$y),
+                  c(0,0))
+
 ggplot() + 
   geom_polygon(data = df_poly1, aes(x,y), fill = black) +
-  geom_segment(data=line,aes(x=x1,y=y1,xend=xend,yend=yend),color = "grey")+
+  geom_segment(data=line,aes(x=x1,y=y1,xend=xend,yend=yend),color = grey)+
   theme_void() +
   theme(plot.margin=margin(1,1,1,1,"cm")) +
   theme(plot.background = element_rect(fill = background , color = background))+
